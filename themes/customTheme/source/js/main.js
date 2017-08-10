c73a0f7969f4d3ad3ac3c249edacda3da1524385
Connor Leech <connorleech@gmail.com>
2017-08-10 09:30:58 -0700

    render post list

--- themes/customTheme/source/js/main.js
+++ themes/customTheme/source/js/main.js
@@ -1,17 +1,19 @@
 // $(function(){
 
 // 	var $tags = $('.tag');
-// 	var $posts = $('.post');
 
 // 	var topicsToDisplay = [];
 
 // 	$tags.on('click', function(e){
 // 		$this = $(this);
 
-// 		// add styling class
+// 		// toggle styling class
 // 		$this.toggleClass('is-primary is-info');
 
 // 		var tagName = $this.attr('class').split(' ')[1];
+
+// 		// show or remove post
+// 		$('.post ' + '.' + tagName);
 
 // 		// add to array if it is not already there
 // 		if ($.inArray(tagName, topicsToDisplay) == -1){
@@ -23,7 +25,6 @@
 // 			}
 // 		}
 
-// 		renderSelectedTags();
 // 	});
 
 // 	function renderAllPosts(){
